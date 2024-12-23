94bf19a9d07e08a663243b059f982a1f3f01541e
Joshua Nolan <64180446+doctorLightbulb@users.noreply.github.com>
2024-12-23 13:39:05 -0800

    Expanded NotesConverter by adding convert() and show_saved_status() methods.

--- notes_converter/notes_converter.py
+++ notes_converter/notes_converter.py
@@ -2,6 +2,8 @@
 engine.
 """
 
+from pathlib import Path
+
 from utils import (
     DATA_PATH,
     FIELD_NAMES,
@@ -15,10 +17,13 @@ from utils import (
 
 class NotesConverter:
     def __init__(self) -> None:
-        self.notes_path = []
+        self.input_path = []
+        self.output_path = ""
 
     def convert(self):
-        merged_notes = load_csv_files(self.notes_path, FIELD_NAMES)
+        self.output_path = Path(self.output_path)
+
+        merged_notes = load_csv_files(self.input_path, FIELD_NAMES)
         notes = build_notes(merged_notes, FIELD_NAMES)
 
         # TODO: Add support for splitting the notes on tag or notebook.
@@ -26,4 +31,13 @@ class NotesConverter:
         title_order = load_json(DATA_PATH / "standard_works_order.json")
         sorted_notes = sort_notes_by_title_and_verse(notes, title_order)
 
-        write_to_docx(sorted_notes, "Gospel Library Notes.docx")
+        write_to_docx(sorted_notes, self.output_path)
+
+        return "".join(self.show_saved_status())
+
+    def show_saved_status(self):
+        return (
+            f"{self.output_path.stem} saved successfully!\n",
+            "File saved in the following location:\n",
+            f"{self.output_path.parent}",
+        )
