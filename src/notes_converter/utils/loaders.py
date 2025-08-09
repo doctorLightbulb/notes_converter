b596a5c70fd30da8f265fc2742732ecb3d6976e5
Joshua Nolan <64180446+doctorLightbulb@users.noreply.github.com>
2025-08-09 15:25:30 -0700

    Added load_csv() and removed extraneous functions as all duplicates will be removed when committing to the temporary database.

--- src/notes_converter/utils/loaders.py
+++ src/notes_converter/utils/loaders.py
@@ -19,48 +19,24 @@ def load_json(path):
 
     Returns
     -------
-    The contents of the `.json` file.
+    The contents of the `.json` file as a dictionary.
     """
     with open(path, "r", encoding="utf-8") as f:
         return json.load(f)
 
 
-def load_csv_as_dict(
-    path: Union[Path, str],
-    field_names: List[str],
-) -> csv.DictReader:
-    """Load a CSV into a `csv.DictReader` iterable.
+def load_csv(path: Path):
+    """Load a `csv` file in `utf-8` encoding, skip the header line
+    and return a generator object.
 
     Parameters
     ----------
-    path : A string or Path object to the file.
-    field_names : A list of strings mapping to the csv fields.
+    path : A string or Path object.
 
     Returns
     -------
-    A `csv.DictReader` iterable.
+    A `csv.generator` object containing a pointer to the `csv` file.
     """
-    return csv.DictReader(open(path, encoding="utf-8"), fieldnames=field_names)
-
-
-@remove_duplicates
-def load_csv_files(files: Sequence[Union[Path, str]], field_names: List[str]):
-    """Load multiple `csv` files and return a merged list.
-
-    Parameters
-    ----------
-    files : A list containing the paths to the files.
-        These can be either `str` or `Path` objects.
-    field_names : A list of strings
-        Containing the fields to which to map the csv columns.
-
-    Returns
-    -------
-    A merged list of `dict`s.
-    """
-    notes = []
-    for file in files:
-        reader = load_csv_as_dict(file, field_names=field_names)
-        next(reader)  # Skip titles (first line)
-        notes += [note for note in reader]
-    return notes
+    reader = csv.reader(open(path, encoding="utf-8"))
+    next(reader)
+    return reader
