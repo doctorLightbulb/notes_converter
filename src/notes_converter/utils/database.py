045eedf0ea27806dc7f0da97e8e14e9036a3abd2
Joshua Nolan <64180446+doctorLightbulb@users.noreply.github.com>
2025-08-09 15:32:12 -0700

    Added Database class, queries and the save_to_database() helper function.

--- src/notes_converter/utils/database.py
+++ src/notes_converter/utils/database.py
@@ -1,22 +1,116 @@
-"""A module containing all functions and classes pertaining to the database."""
+"""A module containing the Database class and related query constants."""
 
 import sqlite3
 from pathlib import Path
-from typing import List, Sequence, Union
 
-from notes_converter.utils.loaders import load_csv_as_dict
+# SETUP QUERIES
+
+CREATE_TABLE_QUERY = """
+    CREATE TABLE IF NOT EXISTS notes(
+        type,
+        title,
+        note_text,
+        source_location,
+        tags,
+        notebooks,
+        study_set,
+        last_updated,
+        created,
+        reference,
+        chapter,
+        verse
+    )
+"""
+
+# COMMIT QUERIES
+
+COMMIT_NOTE_QUERY = """
+    INSERT INTO notes VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+"""
+
+# FETCH QUERIES
+
+FETCH_NOTES = """
+    SELECT type,
+        title,
+        note_text,
+        source_location,
+        tags,
+        notebooks,
+        study_set,
+        last_updated,
+        created,
+        reference,
+        chapter,
+        verse 
+    FROM notes
+    WHERE reference LIKE "%{}%"
+    ORDER BY COALESCE(chapter, verse, created)
+"""
 
 
-def open_database(database):
-    pass
+class Database:
+    """A class for committing data to and fetching data from a SQLite3
+    database.
+    """
+
+    def __init__(self, path):
+        virtual_database = path == ":memory:"
+        if virtual_database:
+            self.db_path = path
+            self.create_empty_table(CREATE_TABLE_QUERY)
+            return
+
+        self.db_path = Path(path)
+
+        database_exists = self.db_path.exists()
+        database_dir_exists = self.db_path.parent.parent.exists()
+
+        if not database_exists and database_dir_exists:
+            self.db_path.parent.mkdir(exist_ok=True)
+            self.create_empty_table(CREATE_TABLE_QUERY)
+
+    @property
+    def db_path(self):
+        return self._db_path
+
+    @db_path.setter
+    def db_path(self, path):
+        self._db_path = path
+
+    def create_empty_table(self, query: str):
+        with sqlite3.connect(self.db_path) as connection:
+            cursor = connection.cursor()
+            cursor.execute(query)
+
+    def commit(self, query, data):
+        """Commit data to the selected database."""
+        with sqlite3.connect(self.db_path) as connection:
+            cursor = connection.cursor()
+            cursor.execute(query, data)
+            connection.commit()
+
+    def commit_many(self, query, data):
+        """Commit data to the selected database."""
+        with sqlite3.connect(self.db_path) as connection:
+            cursor = connection.cursor()
+            cursor.executemany(query, data)
+
+    def update(self, query):
+        """Commit data to the selected database."""
+        with sqlite3.connect(self.db_path) as connection:
+            cursor = connection.cursor()
+            cursor.execute(query)
+            connection.commit()
+
+    def fetch_matches(self, query, word):
+        with sqlite3.connect(self.db_path) as connection:
+            cursor = connection.cursor()
+            results = cursor.execute(query.format(word))
+            return results.fetchall()
 
 
-def commit_to_database(
-    database, files: Sequence[Union[Path, str]], field_names: List[str]
-):
-    for file in files:
-        reader = load_csv_as_dict(file, field_names=field_names)
-        next(reader)  # Skip titles (first line)
-        with sqlite3.connect(database) as conn:
-            cursor = conn.cursor()
-            cursor.execute("INSERT into VALUES(?, ?, ?)")
+def save_to_database(cursor, notes):
+    """Save the current batch of notes to a temporary database."""
+    listed_notes = [i.values() for i in notes]
+    cursor.executemany(COMMIT_NOTE_QUERY, listed_notes)
