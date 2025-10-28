0e18b318e50ed967e6f90a5252e055d775b5873f
Joshua Nolan <64180446+doctorLightbulb@users.noreply.github.com>
2025-10-27 19:04:07 -0700

    Improved docstrings.

--- src/notes_converter/utils/checkers.py
+++ src/notes_converter/utils/checkers.py
@@ -1,5 +1,14 @@
-"""A module containing functions and classes for memory and file size
-inspection.
+"""
+A module containing functions and classes for memory and file size inspection.
+
+Classes
+-------
+- SystemMemory()
+
+Functions
+---------
+- check_file_size()
+- check_required_memory()
 """
 
 from pathlib import Path
@@ -9,8 +18,16 @@ import psutil
 
 
 class SystemMemory:
-    """A convenience class enabling cross-platform memory and storage
-    checking."""
+    """
+    Represents the system memory.
+
+    Methods
+    -------
+    check_memory():
+        Checks available system memory (RAM) against needed memory.
+    check_storage():
+
+    """
 
     def __init__(self) -> None:
         self._memory_info = psutil.virtual_memory()
@@ -18,7 +35,7 @@ class SystemMemory:
         self._current_storage = self._memory_info.total
 
     def check_memory(self, megabytes: float) -> bool:
-        """Check the available system memory (RAM) against `megabytes`.
+        """Checks the available system memory (RAM) against `megabytes`.
 
         Parameters
         ----------
@@ -36,9 +53,9 @@ class SystemMemory:
 
         return True if needed_memory < current_memory else False
 
-    def check_storage(self, megabytes) -> bool:
+    def check_storage(self, megabytes: float) -> bool:
         """
-        Check the available system storage (disk space) against `megabytes`.
+        Checks the available system storage (disk space) against `megabytes`.
 
         Parameters
         ----------
@@ -56,17 +73,48 @@ class SystemMemory:
         return True if needed_storage < current_storage else False
 
 
-def check_file_size(paths: List[str]):
-    """Tally file sizes in bytes and return the sum in megabytes."""
+def check_file_size(paths: List[Path]) -> float:
+    """
+    Tallies file sizes in bytes and returns the sum in megabytes.
+
+    Parameters
+    ----------
+    paths : List[Path]
+        A list of file paths.
+
+    Returns
+    -------
+    float
+        The total size of all files supplied in `paths`.
+    """
     return sum([Path(path).stat().st_size / (1024 * 1024) for path in paths])
 
 
-def check_required_memory(file_path, smu: SystemMemory, overhead: float = 10):
+def check_required_memory(
+    file_paths: List[Path], smu: SystemMemory, overhead: float = 10
+) -> bool:
+    """
+    Checks available memory against required memory for the task.
+
+    Parameters
+    ----------
+    file_paths : List[Path]
+        Contains one or more paths as `str`.
+    smu : SystemMemory
+        The SystemMemory class for the memory check.
+    overhead : float
+        The estimated memory Notes Converter requires to run.
+
+    Returns
+    -------
+    bool
+        Either `True` or `False`.
+    """
     # Estimated memory needed to run the program
     overhead_memory = overhead  # in megabytes
 
     # Estimated memory needed to convert the input file(s)
-    conversion_memory = check_file_size(file_path)  # in megabytes
+    conversion_memory = check_file_size(file_paths)  # in megabytes
     total_memory_needed = overhead_memory + conversion_memory
 
     return smu.check_memory(megabytes=total_memory_needed)
