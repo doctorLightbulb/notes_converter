559dd534d6ecb1a3ddb6676e3b9b8294c85ef6da
Joshua Nolan <64180446+doctorLightbulb@users.noreply.github.com>
2026-04-23 10:24:03 -0700

    Updated convert_datetime() for better formatting. It now uses datetime only.

--- src/notes_converter/utils/converters.py
+++ src/notes_converter/utils/converters.py
@@ -4,8 +4,6 @@ import re
 from datetime import datetime
 from typing import Match
 
-import pytz
-
 MAPPING = {
     ' "': " “",  # Beginning quotation (e.g.,
     '" ': "” ",  # Ending quotation
@@ -34,10 +32,31 @@ def replace_quotes(text: str) -> str:
     return pattern.sub(replace, text)
 
 
-def convert_datetime(note_time: str) -> str:
-    """Converts dates of the form `2026-04-07T15:00:01.654Z` to `April 7, 2026, 08:00 AM PDT`"""
-    default_time = datetime.fromisoformat(note_time.replace("Z", "+00:00"))
-    dt = default_time.replace(tzinfo=pytz.utc)
-    pacific_tz = pytz.timezone("America/Los_Angeles")
-    dt_pacific = dt.astimezone(pacific_tz)
-    return dt_pacific.strftime("%B %e, %Y, %I:%M %p %Z").replace("  ", " ")
+def convert_datetime(note_time: str, timezone: bool = False) -> str:
+    """
+    Converts dates to a human-readable format.
+
+    Given the date `2026-04-07T15:00:01.654Z`, the function returns
+    `April 7, 2026, 8:00 a.m., Pacific Daylight Time`
+
+    Parameters
+    ----------
+    note_time : str
+        Date and time of the form `2026-04-07T15:00:01.654Z`.
+    timezone : bool
+        Whether or not to include the timezone in the output.
+
+    Returns
+    -------
+    str
+        Date and time of the form `April 7, 2026, 8:00 a.m.`
+    """
+
+    raw_datetime = datetime.fromisoformat(note_time)
+    current_timezone = datetime.now().astimezone().tzinfo
+    dt = raw_datetime.astimezone(current_timezone)
+
+    meridian = {"PM": "p.m."}
+    m = meridian.get(f"{dt:%p}", "a.m.")
+    tz = f", {dt:%Z}" if timezone else ""
+    return f"{dt:%B} {dt.day}, {dt:%Y}, {dt.hour%12 or 12}:{dt:%M} {m}{tz}"
